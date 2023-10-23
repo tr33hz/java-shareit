3f80de944e08fc70550b767c0ee761ea073c177b
trhz <treezmusic@yandex.ru>
2023-10-23 14:19:34 +0300

    Создан пакет repository и service в фиче User. Созданы интерфейсы UserRepository и UserService, также их классы имплементации

---
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -0,0 +1,11 @@
+package ru.practicum.shareit.user.model;
+
+import lombok.Data;
+
+/**
+ * TODO Sprint add-controllers.
+ */
+
+@Data
+public class User {
+}
---
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.user.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+}
